36e354a7a05d7b023a85ec93a056c1bd24acbdfd
yleaf <11785335+leavesster@users.noreply.github.com>
2022-09-04 17:11:59 +0800

    support multiple theme and language and add icon for click

--- src/extension.ts
+++ src/extension.ts
@@ -96,7 +96,6 @@ function getWebviewContent(webview: vscode.Webview, context: vscode.ExtensionCon
         <head>
             <meta charset='utf-8'>
             <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
-
             <title>JSONPath generator</title>
             <script>
                 window.addEventListener('load', event => {
